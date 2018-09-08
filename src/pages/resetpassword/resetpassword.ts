0ffcfe44c5a773d3da2b992d533759265f772837
rajnesh <rajneshkis13@gmail.com>
2018-09-08 11:51:21 +0530

    reset passowrd error solved

--- src/pages/resetpassword/resetpassword.ts
+++ src/pages/resetpassword/resetpassword.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { AllserviceProvider } from "../../providers/allservice/allservice";
+import { HelperProvider } from "../../providers/helper/helper";
 /**
  * Generated class for the ResetpasswordPage page.
  *
@@ -14,20 +15,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'resetpassword.html',
 })
 export class ResetpasswordPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  newpassword:string;
+  confirmpassword:string;
+  mobile:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public helper:HelperProvider,public allservice:AllserviceProvider) {
+    this.newpassword='';
+    this.confirmpassword = '';
+    this.mobile = this.navParams.get('mobile');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResetpasswordPage');
   }
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    // this.nav.setRoot(page.component);
-    // this.nav.setRoot(page);
-    //this.active = page;
-    this.navCtrl.push(page);
+  openPage(page) 
+  {
+    if (this.newpassword =='' || this.confirmpassword == '') 
+    {
+      this.helper.simpleToast('please fill above details');
+      return;
+    }
+    if(this.newpassword == this.confirmpassword)
+    {
+      let data = {
+        "method"  : "confirm_password",
+        "from" 	  : "customer",		// restaurant 
+        "mobile_number"  : this.mobile,
+        "password": this.newpassword
+      }
+      this.helper.showSpinner();
+      this.allservice.call(data).subscribe(res => {
+        this.helper.hideSpinner();
+        if (res.status == 200) {
+          this.helper.simpleToast(res.result);
+          this.navCtrl.push(page);
+        }else{
+          this.helper.simpleToast(res.result);
+        }
+      },
+      err => {
+        this.helper.handleRequestError(err);
+      }
+    );
+      
+    }
+    else
+    {
+      this.helper.simpleToast('Password and Confirm password not match');
+      return;
+    }
+    
   } 
 
 }
